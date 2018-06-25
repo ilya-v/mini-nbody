2e3db0426a682d853d15127ca722807ba9f81e4c
Ilya Volkovets <ivolkovets.contractor@libertyglobal.com>
2018-06-25 19:47:55 +0200

    Beautify the n-body code

--- cuda/nbody-orig.cu
+++ cuda/nbody-orig.cu
@@ -6,72 +6,76 @@
 #define BLOCK_SIZE 256
 #define SOFTENING 1e-9f
 
-typedef struct { float x, y, z, vx, vy, vz; } Body;
-
-void randomizeBodies(float *data, int n) {
-  for (int i = 0; i < n; i++) {
-    data[i] = 2.0f * (rand() / (float)RAND_MAX) - 1.0f;
-  }
-}
+typedef struct { float x, y, z, vx, vy, vz; } Particle;
 
 __global__
-void bodyForce(Body *p, float dt, int n) {
-  int i = blockDim.x * blockIdx.x + threadIdx.x;
-  if (i < n) {
-    float Fx = 0.0f; float Fy = 0.0f; float Fz = 0.0f;
+void calcForces(Particle *p, float dt, int n) {
+    int i = blockDim.x * blockIdx.x + threadIdx.x;
+    if (i < n) {
+        float Fx = 0.0f, Fy = 0.0f, Fz = 0.0f;
 
-    for (int j = 0; j < n; j++) {
-      float dx = p[j].x - p[i].x;
-      float dy = p[j].y - p[i].y;
-      float dz = p[j].z - p[i].z;
-      float distSqr = dx*dx + dy*dy + dz*dz + SOFTENING;
-      float invDist = rsqrtf(distSqr);
-      float invDist3 = invDist * invDist * invDist;
+        for (int j = 0; j < n; j++) {
+            float dx = p[j].x - p[i].x;
+            float dy = p[j].y - p[i].y;
+            float dz = p[j].z - p[i].z;
+            float distSqr = dx*dx + dy*dy + dz*dz + SOFTENING;
+            float invDist = rsqrtf(distSqr);
+            float invDist3 = invDist * invDist * invDist;
 
-      Fx += dx * invDist3; Fy += dy * invDist3; Fz += dz * invDist3;
+            Fx += dx * invDist3;
+            Fy += dy * invDist3;
+            Fz += dz * invDist3;
+        }
+
+        p[i].vx += dt*Fx;
+        p[i].vy += dt*Fy;
+        p[i].vz += dt*Fz;
     }
-
-    p[i].vx += dt*Fx; p[i].vy += dt*Fy; p[i].vz += dt*Fz;
-  }
 }
 
 int main(const int argc, const char** argv) {
-  
-  int nBodies = 30000;
-  if (argc > 1) nBodies = atoi(argv[1]);
-  
-  const float dt = 0.01f; // time step
-  const int nIters = 100;  // simulation iterations
 
-  int bytes = nBodies*sizeof(Body);
-  float *buf = (float*)malloc(bytes);
-  Body *p = (Body*)buf;
+    const int
+        N = 30000,
+        nSteps = 100,
+        nBlocks = (N + BLOCK_SIZE - 1) / BLOCK_SIZE;
 
-  randomizeBodies(buf, 6*nBodies); // Init pos / vel data
+    const float dt = 0.01f; // time step
 
-  float *d_buf;
-  cudaMalloc(&d_buf, bytes);
-  Body *d_p = (Body*)d_buf;
+    size_t n_bytes = N*sizeof(Particle);
+    Particle *particles = (Particle*)malloc(n_bytes);
 
-  int nBlocks = (nBodies + BLOCK_SIZE - 1) / BLOCK_SIZE;
-
-  StartTimer();
-  for (int iter = 1; iter <= nIters; iter++) {
-
-    cudaMemcpy(d_buf, buf, bytes, cudaMemcpyHostToDevice);
-    bodyForce<<<nBlocks, BLOCK_SIZE>>>(d_p, dt, nBodies); // compute interbody forces
-    cudaMemcpy(buf, d_buf, bytes, cudaMemcpyDeviceToHost);
-
-    for (int i = 0 ; i < nBodies; i++) { // integrate position
-      p[i].x += p[i].vx*dt;
-      p[i].y += p[i].vy*dt;
-      p[i].z += p[i].vz*dt;
+    for (int i = 0; i < N; i++) {
+        Particle *p = particles + i;
+        p->x = 2.0f * (rand() / (float)RAND_MAX) - 1.0f;
+        p->y = 2.0f * (rand() / (float)RAND_MAX) - 1.0f;
+        p->z = 2.0f * (rand() / (float)RAND_MAX) - 1.0f;
+        p->vx = 0;
+        p->vy = 0;
+        p->vz = 0;
     }
 
-  }
-  double avgTime = GetTimer() / (double)(nIters-1) / 1000.0; 
+    Particle *d_p;
+    cudaMalloc(&d_p, n_bytes);
 
-  printf("N=%d, Titer=%0.3f s\n", nBodies, avgTime);
-  free(buf);
-  cudaFree(d_buf);
+    StartTimer();
+    for (int iter = 1; iter <= nSteps; iter++) {
+
+        cudaMemcpy(d_p, particles, n_bytes, cudaMemcpyHostToDevice);
+        calcForces <<<nBlocks, BLOCK_SIZE>>>(d_p, dt, N);
+        cudaMemcpy(particles, d_p, n_bytes, cudaMemcpyDeviceToHost);
+
+        for (int i = 0 ; i < N; i++) { // integrate position
+            particles[i].x += particles[i].vx*dt;
+            particles[i].y += particles[i].vy*dt;
+            particles[i].z += particles[i].vz*dt;
+        }
+  }
+
+    printf("N=%d, Titer=%0.3f s\n",
+        N,
+        GetTimer() / nSteps / 1000.0);
+
+    free(particles);
+    cudaFree(d_p);
 }
--- nbody.c
+++ nbody.c
@@ -5,7 +5,7 @@
 
 #define SOFTENING 1e-9f
 
-typedef struct { float x, y, z, vx, vy, vz; } Body;
+typedef struct { float x, y, z, vx, vy, vz; } Particle;
 
 void randomizeBodies(float *data, int n) {
   for (int i = 0; i < n; i++) {
@@ -13,7 +13,7 @@ void randomizeBodies(float *data, int n) {
   }
 }
 
-void bodyForce(Body *p, float dt, int n) {
+void bodyForce(Particle *p, float dt, int n) {
   #pragma omp parallel for schedule(dynamic)
   for (int i = 0; i < n; i++) {
     float Fx = 0.0f; float Fy = 0.0f; float Fz = 0.0f;
@@ -41,9 +41,9 @@ int main(const int argc, const char** argv) {
   const float dt = 0.01f; // time step
   const int nIters = 100;  // simulation iterations
 
-  int bytes = nBodies*sizeof(Body);
+  int bytes = nBodies*sizeof(Particle);
   float *buf = (float*)malloc(bytes);
-  Body *p = (Body*)buf;
+  Particle *p = (Particle*)buf;
 
   randomizeBodies(buf, 6*nBodies); // Init pos / vel data
 
